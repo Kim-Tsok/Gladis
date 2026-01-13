c1df41caf49b21405a6574f861835f1b1d836091
Kim-Tsok <appkim02@gmail.com>
2026-01-13 09:33:24 +0100

    Improved instructions for better clarity

--- instructions.py
+++ instructions.py
@@ -1,13 +1,37 @@
 def instructions():
     print("")
-    print("📕 Instructions: ")
-    input("This is a text based choose your adventure game made with python. You press the ENTER key to continue after reading a line of text. ")
-    input("Except when prompted you don't need to answer question cause they won't matter. he only time you should use anything beside the enter key is when you see something like this '(y/n)'. ")
-    input("Besides prompts, there is also combat. You will be informed before entering a combat situation ")
-    input("When in a combat situation, you can use (a) to ATTACK, (d) to DEFEND and (r) to RUN in most battles. ")
-    input("Some battles may play a little different. No matter the change, it will be relayed to you. ")
-    input("Most choices you can make after a question will be shown after the question in a bracket like this.(y/n) ")
-    input("Enjoy Gladys. ")
+    print("📕 Instructions:")
+    input(
+        "This is a text-based choose-your-own-adventure game made with Python.\n"
+        "Press ENTER to continue after reading each line."
+    )
+    input(
+        "Most of the time, you do not need to type anything.\n"
+        "Just press ENTER to move forward."
+    )
+    input(
+        "The only time you should type something is when you see a prompt like (y/n)."
+    )
+    input(
+        "Besides prompts, there is also combat.\n"
+        "You will always be warned before entering a combat situation."
+    )
+    input(
+        "During combat:\n"
+        "(a) = ATTACK\n"
+        "(d) = DEFEND\n"
+        "(r) = RUN\n"
+        "Most battles follow these rules."
+    )
+    input(
+        "Some battles may work differently.\n"
+        "Any changes will be explained when they happen."
+    )
+    input(
+        "Choices are usually shown in brackets, like this (y/n)."
+    )
+    print("")
+    input("Enjoy the journey, Gladys. 💀⚔️")
     print("")
     print("=" * 10)
-    print(" ")
+    print("")
